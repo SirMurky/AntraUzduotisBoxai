e23a426abc37aa0b7a10dc337db8a9aebf891cda
RonaldJaz <31950897+RonaldJaz@users.noreply.github.com>
2017-12-11 10:18:56 +0200

    Pridetos spalvu keitimosi funkcijos

--- App.js
+++ App.js
@@ -26,3 +26,35 @@ var spalva1 = 'green';
 var spalva2 = 'white';
 var spalva3 = 'blue';
 var spalva4 = 'yellow';
+export default class App extends Component<{}> {
+  constructor(){
+    super() 
+      this.state =
+      {
+       spalvaA : 'red',
+       spalvaB : 'red',
+       spalvaC : 'red',
+       spalvaD : 'red',
+	   spalvaE: 'green'
+      }
+    }
+    spalvaKeiciasi()
+    {
+      var backgroundColor = spalva1;
+      this.setState({spalvaA: backgroundColor})
+    }
+    spalvaKeiciasi1()
+    {
+      var backgroundColor = spalva2;
+      this.setState({spalvaB: backgroundColor})
+    }
+    spalvaKeiciasi2()
+    {
+      var backgroundColor = spalva3;
+      this.setState({spalvaC: backgroundColor})
+    }
+    spalvaKeiciasi3()
+    {
+      var backgroundColor = spalva4;
+      this.setState({spalvaD: backgroundColor})
+    }
