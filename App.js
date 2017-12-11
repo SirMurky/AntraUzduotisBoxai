d0fcf2e039e87f0f09c96e1c8d635803a7855d92
RonaldJaz <31950897+RonaldJaz@users.noreply.github.com>
2017-12-11 10:18:01 +0200

    Pridetos konstantos

--- App.js
+++ App.js
@@ -13,3 +13,16 @@ import {
   TouchableOpacity
  
 } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+var spalva1 = 'green';
+var spalva2 = 'white';
+var spalva3 = 'blue';
+var spalva4 = 'yellow';
